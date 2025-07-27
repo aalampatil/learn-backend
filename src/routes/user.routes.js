befd953c04b8ab508622959fcb4bd26fedd78f40
aalam <aalam.workacc@gmail.com>
2025-07-27 22:23:07 +0530

    refactor user routes; organize imports and improve code formatting for readability

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { changePassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails } from "../controllers/user.controller.js";
+import {
+  changePassword,
+  getCurrentUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateUserCoverImage,
+  updateUserAvatar,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -9,24 +19,25 @@ router.route("/register").post(
   upload.fields([
     {
       name: "avatar",
-      maxCount: 1
+      maxCount: 1,
     },
     {
       name: "coverImage",
-      maxCount: 1
-    }
+      maxCount: 1,
+    },
   ]),
   registerUser
 );
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured route
-router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh-tokens").post(refreshAccessToken)
-router.route("/change-password").post(verifyJWT, changePassword)
-router.route("/current-user").get(verifyJWT, getCurrentUser)
-router.route("/update-account-details").post(verifyJWT, updateAccountDetails)
-
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-tokens").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changePassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account-details").post(verifyJWT, updateAccountDetails);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 export default router;
